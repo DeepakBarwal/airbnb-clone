2adf17e32a17c9c072fd7187506f101fcf9820a9
DeepakBarwal <deepakbarwal98@gmail.com>
2023-11-09 11:31:16 +0530

    styled browse listings page

--- app/host/[listingId]/page.tsx
+++ app/host/[listingId]/page.tsx
@@ -5,6 +5,7 @@ import { StarIcon, MapPinIcon } from "@heroicons/react/24/solid";
 import Calendar from "./components/Calendar";
 import ListingImage from "./components/ListingImage";
 import { revalidatePath } from "next/cache";
+import ImageUploader from "../components/ImageUploader";
 
 export default async function Page({
   params,
@@ -39,28 +40,35 @@ export default async function Page({
   }
 
   return (
-    <div>
-      <h1>{listing.name}</h1>
-      {listing.image ? (
-        <ListingImage
-          imgUrl={listing.image}
-          imgAlt={listing.name}
-          saveFileUrl={saveFileUrl}
-        />
-      ) : null}
-      <p>{listing.description}</p>
-      <p>{listing.price}</p>
-      {listing.rating ? (
+    <>
+      <h1 className="text-4xl my-4 font-semibold text-center">
+        Modify Your Listing
+      </h1>
+      <div className="container md:max-w-xl mx-auto border rounded-md p-4 mt-4">
+        <h2 className="text-2xl my-4 font-semibold">{listing.name}</h2>
+        {listing.image ? (
+          <ListingImage
+            imgUrl={listing.image}
+            imgAlt={listing.name}
+            saveFileUrl={saveFileUrl}
+          />
+        ) : (
+          <ImageUploader saveFileUrl={saveFileUrl} />
+        )}
+        <p className="text-slate-700 my-2">{listing.description}</p>
+        <p className="font-bold my-2">₹{listing.price}/night</p>
+        {listing.rating ? (
+          <div className="flex flex-row items-center">
+            <StarIcon className="h-5 w-5 text-gray-800 mr-1" />
+            <span className="text-gray-800">{listing.rating}</span>
+          </div>
+        ) : null}
         <div className="flex flex-row items-center">
-          <StarIcon className="h-5 w-5 text-gray-800 mr-1" />
-          <span className="text-gray-800">{listing.rating}</span>
+          <MapPinIcon className="h-5 w-5 text-gray-800 mr-1" />
+          <span className="text-gray-800">{listing.location}</span>
         </div>
-      ) : null}
-      <div className="flex flex-row items-center">
-        <MapPinIcon className="h-5 w-5 text-gray-800 mr-1" />
-        <span className="text-gray-800">{listing.location}</span>
+        <Calendar listing={listing} />
       </div>
-      <Calendar listing={listing} />
-    </div>
+    </>
   );
 }
